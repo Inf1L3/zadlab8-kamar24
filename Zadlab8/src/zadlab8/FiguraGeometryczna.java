23f931d0252469b14bb1c5ec9863a70520d9ecc9
Lukasz <Lukasz@Komp>
2018-05-15 20:19:22 +0200

    Zadaniaza

---
+++ Zadlab8/src/zadlab8/FiguraGeometryczna.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zadlab8;
+
+/**
+ *
+ * @author Lukasz
+ */
+public abstract class FiguraGeometryczna {
+   
+    public abstract double obliczPole();
+}
